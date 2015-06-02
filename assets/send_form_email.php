a89579981c110d0c05271ac9a43c136ec2b42968
Lauren Achtem <laurenachoo@users.noreply.github.com>
2015-06-02 11:54:43 -0700

    Update send_form_email.php

--- assets/send_form_email.php
+++ assets/send_form_email.php
@@ -1,136 +1,32 @@
 <?php
- 
-if(isset($_POST['email'])) {
- 
-     
- 
-    $email_to = "achtemlauren@gmail.com";
- 
-    $email_subject = "Someone contacted you from your Portfolio site!";
- 
-     
- 
-     
- 
-    function died($error) {
- 
-        // error code can go here
- 
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
- 
-        echo "These errors appear below.<br /><br />";
- 
-        echo $error."<br /><br />";
- 
-        echo "Please go back and fix these errors.<br /><br />";
- 
-        die();
- 
-    }
- 
-     
- 
-    // validation expected data exists
- 
-    if(!isset($_POST['name']) ||
- 
-        !isset($_POST['email']) ||
- 
-        !isset($_POST['message'])) {
- 
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
- 
-    }
- 
-     
- 
-    $name = $_POST['name']; // required
- 
-    $email_from = $_POST['email']; // required
- 
-    $message = $_POST['message']; // required
- 
-     
- 
-    $error_message = "";
- 
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
- 
-  if(!preg_match($email_exp,$email_from)) {
- 
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
- 
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp,$name)) {
- 
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
- 
-  }
- 
- 
-  if(strlen($message) < 2) {
- 
-    $error_message .= 'The Message you entered do not appear to be valid.<br />';
- 
-  }
- 
-  if(strlen($error_message) > 0) {
- 
-    died($error_message);
- 
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
- 
-      $bad = array("content-type","bcc:","to:","cc:","href");
- 
-      return str_replace($bad,"",$string);
- 
-    }
- 
-     
- 
-    $email_message .= "Name: ".clean_string($name)."\n";
- 
-    $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Message: ".clean_string($message)."\n";
- 
-     
- 
-     
- 
-// create email headers
- 
-$headers = 'From: '.$email_from."\r\n".
- 
-'Reply-To: '.$email_from."\r\n" .
- 
-'X-Mailer: PHP/' . phpversion();
- 
-@mail($email_to, $email_subject, $email_message, $headers);  
- 
-?>
- 
- 
- 
-<!-- own success html here -->
- 
- 
- 
-Thank you for contacting me! I will be in touch with you very soon!
- 
- 
- 
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'achtemlauren@gmail.com';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly.');
+		window.location = 'contact_page.html';
+	</script>
 <?php
- 
 }
- 
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to gordon@template-help.com');
+		window.location = 'contact_page.html';
+	</script>
+<?php
+}
 ?>
